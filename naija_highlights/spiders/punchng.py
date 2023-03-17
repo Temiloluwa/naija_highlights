82b03c7ac42d5ebde80cf72db6f256ee9c79adc3
Temiloluwa <temilolu74@yahoo.co.uk>
2023-03-17 06:56:30 +0100

    initial commit naija highlights

---
+++ naija_highlights/spiders/punchng.py
@@ -0,0 +1,41 @@
+import pytz
+from datetime import datetime
+from scrapy.spiders import SitemapSpider
+from naija_highlights.items import NaijaHighlightsItem
+
+def localize_time(dt: datetime) -> datetime:
+    """ localize time to Lagos """
+    if dt.tzinfo:
+        return dt.replace(tzinfo=pytz.timezone('Africa/Lagos'))
+    return pytz.timezone('Africa/Lagos').localize(dt)
+
+class PunchNgSpider(SitemapSpider):
+    name = "punchng"
+    sitemap_urls = ["https://punchng.com/sitemap-posttype-post.2023.xml"]
+
+    def sitemap_filter(self, entries):
+        _, this_week, _ = localize_time(datetime.today()).isocalendar()
+        for entry in entries:
+            _, publish_week, _ = localize_time(datetime.\
+                    strptime(entry['lastmod'], '%Y-%m-%dT%X%z')).isocalendar()
+            if publish_week == this_week:
+                yield entry
+            
+
+    def parse(self, response):
+        post = NaijaHighlightsItem()
+        article =  response.css(".single-article")
+        post["title"] = article.css("h1::text").extract()
+        body = ""
+        for paragraph in article.css("p"):
+            body += paragraph.extract()
+
+        post["body"] = body
+
+        post["weblink"] = None
+        post["title"] = None
+        post["thumbnaillink"] = None
+        post["author"] = None
+        post["body"] = None
+
+        yield post
