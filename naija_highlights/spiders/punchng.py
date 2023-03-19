57aebe671dfac563b3fdef94918ae9c1d113df6c
Temiloluwa <temilolu74@yahoo.co.uk>
2023-03-19 16:44:58 +0100

    completed punchng for now

--- naija_highlights/spiders/punchng.py
+++ naija_highlights/spiders/punchng.py
@@ -1,9 +1,25 @@
+import sys
+import logging
 import pytz
 import re
+import os
 from typing import Tuple
 from datetime import datetime
 from scrapy.spiders import SitemapSpider
 from naija_highlights.items import NaijaHighlightsItem
+
+log_dir = os.path.join(os.path.dirname(__file__), 'data', 'logs')
+log_format = logging.Formatter("%(asctime)s %(filename)-12s %(levelname)-8s %(message)s")
+os.makedirs(log_dir, exist_ok=True)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_handler.setFormatter(log_format)
+logger.addHandler(stream_handler)
+file_handler = logging.FileHandler(os.path.join(log_dir, f"logfile.log"))
+file_handler.setFormatter(file_handler)
+logger.addHandler(file_handler)
+
 
 def localize_time(dt: datetime) -> datetime:
     """ localize time to Lagos """
@@ -17,30 +33,38 @@ def preprocess_postdate(dt: str) -> Tuple[int, int, int]:
     dt = dt.strip("\n").strip(" ")
     dt = re.sub(r'(th)|(st)|(nd)', '', dt)
     dt = datetime.strptime(dt, '%d %B %Y')
-    return dt.day, dt.month, dt.year
+    return dt
+
+def get_this_week():
+    _, this_week, _ = localize_time(datetime.today()).isocalendar()
+    return this_week
 
 class PunchNgSpider(SitemapSpider):
     name = "punchng"
     sitemap_urls = ["https://punchng.com/sitemap-posttype-post.2023.xml"]
+    this_week = get_this_week()
 
     def sitemap_filter(self, entries):
-        _, this_week, _ = localize_time(datetime.today()).isocalendar()
         for entry in entries:
             _, publish_week, _ = localize_time(datetime.\
                     strptime(entry['lastmod'], '%Y-%m-%dT%X%z')).isocalendar()
-            if publish_week == this_week:
+            if publish_week == self.this_week:
                 yield entry
-            
+
 
     def parse(self, response):
         post = NaijaHighlightsItem()
         article =  response.css(".single-article")
         post["weblink"] = response.url
         post["title"] = article.css("h1::text").get()
-        post["postdate"] = preprocess_postdate(article.css(".post-date::text").get())
+        dt = preprocess_postdate(article.css(".post-date::text").get())
+        _, week_number, _ = dt.isocalendar()
+        post["postdate"] = (dt.day, dt.month, dt.year) 
         post["thumbnaillink"] = response.xpath("//figure/img/@src").get()
         post["author"] = response.xpath("//span[@class='post-author']/a/text()").get()
         post["body"] = article.css(".post-content").xpath("p").getall()
 
-        yield post
+        # ascertain that post was created this week
+        if week_number == self.this_week:
+            yield post
     
